bee8362c19087f6adfb4c6f265186a9b17067172
Allan Dellon <123901220+dellonx@users.noreply.github.com>
2024-11-08 09:43:03 -0300

    Add files via upload

---
+++ Galeria/script.js
@@ -0,0 +1,8 @@
+let figuras = {
+    'https://img.elo7.com.br/product/685x685/45A7149/caneca-meme-bolsotse-bolsonaro-comunista-canecas-engracadas.jpg'
+    'https://th.bing.com/th/id/OIP.haXqudYFuyo-e83qD5MB6AHaGM?rs=1&pid=ImgDetMain'
+    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR1u0jm0BLEFpm1z5ElTqcH9WzRy3Ki-4eoeQ&s'
+}
+function mudaImagem {
+    console.log('oi')
+}
